a856355d908f80fff6cbde4911982353aa88744a
kingwhangzang <soyun011112@sookmyung.ac.kr>
2025-02-06 17:16:55 +0900

    UserManager 추가

---
+++ work3/Main.java
@@ -0,0 +1,23 @@
+package work3;
+
+public class Main {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+
+        IncrementThread t1 = new IncrementThread(counter);
+        IncrementThread t2 = new IncrementThread(counter);
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Final Counter Value: " + counter.getValue());
+        System.out.println("Expected Value: 2000 (If no race condition occurs)");
+    }
+}
