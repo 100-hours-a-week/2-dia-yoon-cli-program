2b5db3704724f64c872bbbccc4148f74452f4dc8
kingwhangzang <soyun011112@sookmyung.ac.kr>
2025-02-09 23:41:30 +0900

    2주차 수업 중 과제코드

---
+++ work3/Counter.java
@@ -0,0 +1,13 @@
+package work3;
+
+public class Counter {
+    private int value = 0;
+
+    public synchronized void increment() {
+        value++;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
---
+++ work3/IncrementThread.java
@@ -0,0 +1,16 @@
+package work3;
+
+public class IncrementThread extends Thread {
+    private Counter counter;
+
+    public IncrementThread(Counter counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            counter.increment();
+        }
+    }
+}
